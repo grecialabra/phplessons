7a0bf2e6b5feb9096434397d94c174823b687a5b
Grecia Labra <grecia.zoe@icloud.com>
2024-03-22 08:44:04 -0500

    egeg

---
+++ datatypes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+</form>
+    <?php
+ //String
+ $name = 'I like to read';
+
+ //Integer
+ $name = 20;
+
+ //Float
+ $name = 20.5784;
+
+ //BooLean
+ 
+
+ //Array
+ $names = array("Zoe","Grecia","Labra");
+ echo $names['2']
+    ?>
+</body>
+</html>
