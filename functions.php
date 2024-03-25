c2064c74167802b1a0f2917486dbd0e6b8f58822
Grecia Labra <grecia.zoe@icloud.com>
2024-03-25 08:33:48 -0500

    thrh

--- functions.php
+++ functions.php
@@ -3,13 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 <body>
-   
     <?php
- echo str_replaced("grecia","kelli","hi grecia");
 
+    echo str_replaced("grecia","kelli","hi grecia");
     ?>
 </body>
-</html>
