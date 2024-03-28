6878c49200e474c193d9d309633e2d059334aedc
Grecia Labra <grecia.zoe@icloud.com>
2024-03-28 08:10:35 -0500

    4tgt4t

---
+++ calendar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="UTF-8">
+    <title> Title of the document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+<?php
+    $dayofweek = date("w");
+    
+    switch ($dayofweek) {
+        case 1:
+            echo "It is Monday!";
+        break;   
+        case 2:
+            echo "It is Tuesday!";
+        break;   
+        case 3:
+            echo "It is Wednesday!";
+        break;   
+        case 4:
+            echo "It is Thursday!";
+        break;   
+        case 5:
+            echo "<p>It is Friday!</p>";
+        break;   
+        case 6:
+            echo "It is Saturday!";
+        break;   
+        case 0:
+            echo "It is Sunday!";
+        break;   
+    } 
+?>
+
+</body>
+
+</html>
