ba6ca635c63e8d8515440c8faa78929cc7cda164
Grecia Labra <grecia.zoe@icloud.com>
2024-04-02 07:21:35 -0500

    wrtrht

---
+++ conditionals.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+ 
+<body>
+<?php
+    $x = 1;
+    if ($x == 1) {
+          echo "Daniel is very handsome!";
+    }
+    elseif ($x == 2) {
+        echo "Daniel is kinda handsome!";
+    }
+    elseif ($x == 3) {
+        echo "Daniel is kinda handsome!";
+    }
+    elseif ($x == 4) {
+        echo "Daniel is kinda handsome!";
+    }
+    else  {
+        echo "Daniel is very ugly!";
+    }
+    ?>
+    
+ 
+    </body>
+ 
+    </html>
